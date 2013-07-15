1b98627f0626d9d0b0dfd04c3dc610b44d59b2b8
hechao <hechaoyuyu@gmail.com>
2013-07-15 21:47:34 +0800

    android adb tools

---
+++ usb_vendor_ids.c
@@ -0,0 +1,63 @@
+int builtInVendorIds[] = {
+    0x18d1, /* Google */
+	 0x8087, /* INTEL */
+    0x0bb4, /* HTC */
+    0x04e8, /* Samsung */
+    0x22b8, /* Motorola */
+    0x1004, /* LG */
+    0x12d1, /* Huawei */
+    0x0502, /* Acer */
+    0x0fce, /* Sony Ericsson */
+    0x0489, /* Foxconn */
+    0x413c, /* Dell */
+    0x0955, /* Nvidia */
+    0x091e, /* Garmin-Asus */
+    0x04dd, /* Sharp */
+    0x0482, /* Kyocera */
+    0x10a9, /* Pantech */
+    0x05c6, /* Qualcomm */
+    0x2257, /* On-The-Go-Video */
+    0x0409, /* NEC */
+    0x04da, /* Panasonic Mobile Communication */
+    0x0930, /* Toshiba */
+    0x1f53, /* SK Telesys */
+    0x2116, /* KT Tech */
+    0x0b05, /* Asus */
+    0x0471, /* Philips */
+    0x0451, /* Texas Instruments */
+    0x0f1c, /* Funai */
+    0x0414, /* Gigabyte */
+    0x2420, /* IRiver */
+    0x1219, /* Compal */
+    0x1bbb, /* T & A Mobile Phones */
+    0x2006, /* LenovoMobile */
+    0x17ef, /* Lenovo */
+    0xe040, /* Vizio */
+    0x24e3, /* K-Touch */
+    0x0e79, /* Archos */
+    0x1662, /* Positivo */
+    0x15eb, /* VIA-Telecom */
+    0x04c5, /* Fujitsu */
+	 0x25e3, /* Lumigon */
+    0x0408, /* Quanta */
+    0x2314, /* INQ Mobile */
+    0x1949, /* Lab126 */
+	 0x1ebf, /* Yulong Coolpad */
+	 0x2237, /* Kobo */
+    0x16d5, /* AnyDATA */
+    0x109b, /* Hisense */
+    0x2080, /* Nook */
+    0x1d4d, /* Pegatron */
+    0x054c, /* Sony */
+    0x2340, /* Teleepoch */
+    0x19d2, /* ZTE */
+    0x201e, /* Haier */
+    0x2717, /* Xiaomi */
+    0x0e8d, /* MTK */
+    0x22da, /* MEEG */
+    0x22d9, /* OPPO */
+    0x19d1, /* BYD */
+	 0x2836, /* OUYA */
+	 0x1d45, /* Qisda */
+    /* TODO: APPEND YOUR ID HERE! */
+};
